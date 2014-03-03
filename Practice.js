d283e29db9a4c18724177a48283d4012c6141837
George Henry <jegiuffrafacio@gmail.com>
2014-03-03 06:09:56 -0200

    h

--- Practice.js
+++ Practice.js
@@ -1,9 +1,34 @@
-var pares=[2,4,,6,8],
-suma=0;
+var $form = $("#formulario"),
+	$titulo = $("#titulo"),
+	$url = $("#url"),
+	$button = $("#mostrar-form"),
+	$list = $('#contenido'),
+	$post =  $(".item").first();//contenido
 
-for(vari=0;i<pares.length;i++){
-	suma=suma+pares[i];
+function mostrarFormulario(){
+	$form.slideToggle();
+	return false;
 }
 
+function agregarPost(){
+	var url = $url.val(),
+		titulo = $titulo.val(),
+		$clone = post.clone(); //jquery me da una representacion del objeto
 
+
+	$clone.find(".titulo_item a")//encontrar elementos que cumplan igual css	
+		.text(titulo)//modificar el titulo
+		.attr('href', url);//remplazamos el html
+
+	$clone.hide();//atajo para ocultar al clon
+
+	$list.prepend($clone);//embeber el clon creado y ponerlo primero
+	$clone.fadeIn();//efecto de aparecer
+	
+	return false;
+}
+
+//Eventos
+$button.click(mostrarFormulario);
+$form.on('submit', agregarPost);
   
