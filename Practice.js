8496a675b8b5ef523ab3671293c8f56e2a50ee19
George Henry <jegiuffrafacio@gmail.com>
2014-03-05 21:55:51 -0200

    k

--- Practice.js
+++ Practice.js
@@ -5,16 +5,22 @@ var $form = $("#formulario"),
 	$list = $('#contenido'),
 	$post =  $(".item").first();//contenido
 
-var $form = $('#formulario'),
-	$titulo = $('#titulo'),
-	$url = $('#url'),
-	$button = $('#mostrar-form'),
-	$list = $('#contenido'),
-	$post = $('.item').first();
 
+// para guardar tit y url x si se me va la pag y no subi el post.
+if (localStorage.getItem('autosave')){//Si tiene un item gurdado es true y sigue
+	$titulo.val(sesionStorage.getItem('titulo'));
+	$url.val(sesionStorage.getItem('url')); 
+}
+
+//guardar lo que esta tipeado en cada campo url y titulo. Cada un seg JS ejecuta la funcion
+var id = setInterval(function(){//
+	sesionStorage.setItem('titulor', $titulo.val());
+	sesionStorage.setItem('url'), $url.val());
+}, 1000/*milisegundos de guardado de tipeado*/);
 
 function mostrarFormulario(){
 	$form.slideToggle();
+	$list.slideToggle();
 	return false;
 }
 
@@ -30,8 +36,10 @@ function agregarPost(){
 	$clone.hide();
 
 	$list.prepend($clone);
-
-	$clone.fadeIn();
+	mostrarFormulario();
+	$titulo.val('');// si al .val le pasamos un valor vacio le agregamos un valor
+	$url.val('');
+	$clone.slideDown();
 
 	return false;
 }
